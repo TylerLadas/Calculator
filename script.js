4f6ad9546a171ed16a5c39e4ef6cfbeb88adbdb6
TylerLadas <tylerladas@hotmail.com>
2022-01-31 15:39:41 -0500

    final adjustments

--- script.js
+++ script.js
@@ -42,10 +42,10 @@ class Calculator {
             case '-':
                 compututation = prev - current
                 break
-            case '&times;':
+            case 'x':
                 compututation = prev * current
                 break
-            case '&divide;':
+            case '÷':
                 compututation = prev / current
                 break
             default: 
@@ -56,9 +56,32 @@ class Calculator {
         this.previousOperand = ''
     }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        }  else {
+            this.previousOperandTextElement.innerText = ''
+        }
     }
 }
 
