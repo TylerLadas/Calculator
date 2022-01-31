08dcaa28224bb5cfe2fc98ba7fa2a8889f3f53da
TylerLadas <tylerladas@hotmail.com>
2022-01-31 12:02:35 -0500

    initial JS

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-document.
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
